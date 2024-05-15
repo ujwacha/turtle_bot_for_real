4017487cd08fe9fd74f53646a8d2f32f37129d05
Suyog Bhandari <079bct091.suyog@pcampus.edu.np>
2024-05-15 18:21:46 +0545

    hello world

--- Core/Inc/driver.hpp
+++ Core/Inc/driver.hpp
@@ -2,20 +2,21 @@
 #include "stm32f407xx.h"
 #include "stm32f4xx_hal_gpio.h"
 #include "stm32f4xx_hal_tim.h"
-class Driver{
-  public:
+class Driver
+{
+ public:
   Driver(GPIO_TypeDef* dchannel, uint16_t dpin, TIM_HandleTypeDef* tim_pwm, uint32_t channel, int m_pwm, int8_t pdir);
   void run_motor(float percentage, GPIO_PinState dir);
 
 
-  private:
+ private:
   GPIO_PinState get_pinstate(GPIO_PinState current); // sets the positive direction of spin for the motor
 
   GPIO_TypeDef* dir_channel; // dicection pin channel
   uint16_t dir_pin; // direction pin
 
-  
-  
+
+
   TIM_HandleTypeDef* timer_pwm; // timer handler to set pwm
   uint32_t timer_channel; // channel for the pwm
   int max_pwm; // max pwm value
