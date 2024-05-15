a024166ee4b84c17f79e77a184986955dfe08c62
ujwacha <ujwolacharya4@gmail.com>
2024-05-15 16:38:16 +0545

    Made the encoder compilable
    you just had to include tim.h lol

--- Core/Inc/encoder.hpp
+++ Core/Inc/encoder.hpp
@@ -1,19 +1,19 @@
-#include <cstdint>
-#include <stm32f4xx_hal_tim.h>
 #define PI 3.14159265358979f
+#include "tim.h"
 
-class encoder
+class Encoder
 {
- private:
-  TIM_HandleTypeDef *henc; // pointer to encoder timer
+public:
+  TIM_HandleTypeDef *henc;
   int16_t cpr = 0; // count per revolution
   uint16_t sample_time; // time to sample for omega
   float omega = 0.0f;
   int64_t count_aggregate = 0;
   uint32_t last_reset_time = 0;
+  int16_t error;
 
  public:
-  encoder(TIM_HandleTypeDef *_henc, int16_t _cpr, uint16_t _sample_time = 1)
+  Encoder(TIM_HandleTypeDef *_henc, int16_t _cpr, uint16_t _sample_time = 1)
   {
    henc = _henc;
    cpr = _cpr;
@@ -24,7 +24,7 @@ class encoder
   //  ~encoder() = default;
   void init();
   float get_omega();
-  int32_t get_count();
+  int16_t get_count();
   int64_t get_count_aggregate();
   void reset_encoder_count();
 };
