f0e7b3662e7ebd5f6f8748364483fe3b0e2b16d1
ujwacha <ujwolacharya4@gmail.com>
2024-05-16 10:55:20 +0545

    Changed the kinematics convention to stupid piece of shit

--- Core/Inc/robot.hpp
+++ Core/Inc/robot.hpp
@@ -42,8 +42,8 @@ class Robot {
 
   Driver m1_driver = Driver(motor_dir_ports[0],motor_dir_pins[0],motor_pwm_timers[0],motor_pwm_timer_channels[0],max_pwm[0],1);
   Driver m2_driver = Driver(motor_dir_ports[3],motor_dir_pins[3],motor_pwm_timers[3],motor_pwm_timer_channels[3],max_pwm[3],1);
-  Driver m3_driver = Driver(motor_dir_ports[1],motor_dir_pins[1],motor_pwm_timers[1],motor_pwm_timer_channels[1],max_pwm[1],-1);
-  Driver m4_driver = Driver(motor_dir_ports[2],motor_dir_pins[2],motor_pwm_timers[2],motor_pwm_timer_channels[2],max_pwm[2],-1);
+  Driver m3_driver = Driver(motor_dir_ports[1],motor_dir_pins[1],motor_pwm_timers[1],motor_pwm_timer_channels[1],max_pwm[1],1);
+  Driver m4_driver = Driver(motor_dir_ports[2],motor_dir_pins[2],motor_pwm_timers[2],motor_pwm_timer_channels[2],max_pwm[2],1);
 
   Robot() {
 
