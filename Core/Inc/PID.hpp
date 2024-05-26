e239e52178090edb06175462aeed164de630de57
ujwacha <ujwolacharya4@gmail.com>
2024-05-26 22:56:44 +0545

    new PID controller

--- Core/Inc/PID.hpp
+++ Core/Inc/PID.hpp
@@ -1,95 +1,50 @@
-#ifndef PID_v1_h
-#define PID_v1_h
-#define LIBRARY_VERSION 1.2.1
-
+#include "stm32f4xx_hal.h"
+#include <cstdint>
 class PID {
+public:
+  /* Controller gains */
+  float Kp;
+  float Ki;
+  float Kd;
+  
+  /* Derivative low-pass filter time constant */
+  float tau;
+  
+  /* Output limits */
+  float limMin;
+  float limMax;
+  
+  /* Integrator limits */
+  float limMinInt;
+  float limMaxInt;
+  
+  /* Sample time (in seconds) */
+  float T;
+  
+  /* Controller "memory" */
+  float integrator = 0.0f;
+  float prevError = 0.0f;			/* Required for integrator */
+  float differentiator = 0.0f;
+  float prevMeasurement = 0.0f;		/* Required for differentiator */
+  uint32_t prev_time;
+  /* Controller output */
+  float out = 0.0f;
+  
+  PID(float _kp, float _ki, float _kd,
+      float _T=0.030,
+      float _tau = 0.01,
+      float _min = -499.0f , float _max = 499.0f,
+      float _intmin = -400.0f, float _intmax = +400.0f);
 
- public:
-  // Constants used in some of the functions below
-#define AUTOMATIC 1
-#define MANUAL 0
-#define DIRECT 0
-#define REVERSE 1
-#define P_ON_M 0
-#define P_ON_E 1
 
-  // commonly used functions
-  // **************************************************************************
-  PID(double *, double *,
-double *, // * constructor.  links the PID to the Input, Output, and
-double, double, double, int,
-int); //   Setpoint.  Initial tuning parameters are also set here.
- //   (overload for specifying proportional mode)
+  float set_limits(float _min , float _max);
 
-  PID(double *, double *,
-double *, // * constructor.  links the PID to the Input, Output, and
-double, double, double,
-int); //   Setpoint.  Initial tuning parameters are also set here
+  float set_int_limits(float _intmin , float _intmax );
 
-  void SetMode(int Mode); // * sets PID to either Manual (0) or Auto (non-0)
+  float set_sampling_time(float _T);
 
-  bool Compute(); // * performs the PID calculation.  it should be
- //   called every time loop() cycles. ON/OFF and
- //   calculation frequency can be set using SetMode
- //   SetSampleTime respectively
+  float set_tau(float _tau);
 
-  void SetOutputLimits(
-double,
-double); // * clamps the output to a specific range. 0-255 by default, but
-//   it's likely the user will want to change this depending on
-//   the application
+  float PIDController_Update(float setpoint, float measurement);
 
-// available but not commonly used functions
-// ********************************************************
-  void SetTunings(
-double, double, // * While most users will set the tunings once in the
-double);        //   constructor, this function gives the user the option
-//   of changing tunings during runtime for Adaptive control
-  void SetTunings(double, double, // * overload for specifying proportional mode
-double, int);
-
-  void SetControllerDirection(
-int); // * Sets the Direction, or "Action" of the controller. DIRECT
- //   means the output will increase when error is positive. REVERSE
- //   means the opposite.  it's very unlikely that this will be
- //   needed once it is set in the constructor.
-  void
-   SetSampleTime(int); // * sets the frequency, in Milliseconds, with which
-  //   the PID calculation is performed.  default is 100
-
-  // Display functions
-  // ****************************************************************
-  double GetKp();     // These functions query the pid for interal values.
-  double GetKi();     //  they were created mainly for the pid front-end,
-  double GetKd();     // where it's important to know what is actually
-  int GetMode();      //  inside the PID.
-  int GetDirection(); //
-
- private:
-  void Initialize();
-
-  double dispKp; // * we'll hold on to the tuning parameters in user-entered
-  double dispKi; //   format for display purposes
-  double dispKd; //
-
-  double kp; // * (P)roportional Tuning Parameter
-  double ki; // * (I)ntegral Tuning Parameter
-  double kd; // * (D)erivative Tuning Parameter
-
-  int controllerDirection;
-  int pOn;
-
-  double *myInput;  // * Pointers to the Input, Output, and Setpoint variables
-  double *myOutput; //   This creates a hard link between the variables and the
-  double
-   *mySetpoint; //   PID, freeing the user from having to constantly tell us
-//   what these values are.  with pointers we'll just know.
-
-  unsigned long lastTime;
-  double outputSum, lastInput;
-
-  unsigned long SampleTime;
-  double outMin, outMax;
-  bool inAuto, pOnE;
 };
-#endif
